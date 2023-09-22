6a61835be9eee2d70bed6951748cd78c683f7db5
Minh-TanTRAN <minh-tan.tran@epfedu.fr>
2023-09-22 15:08:32 +0200

    Google analytic

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-P8H5EB609C"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' G-P8H5EB609C');
+    </script>
+    """
+    return prefix_google + "Hello World"
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
