d337f3979351a3041f3488d49c7c354949da5ba6
Minh-TanTRAN <124440648+Minh-TanTRAN@users.noreply.github.com>
2023-10-14 21:58:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 import logging
 import requests
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.analytics.data_v1beta.types import RunReportRequest
+import os
+
+
 
 app = Flask(__name__)
 
@@ -68,8 +73,16 @@ def logger():
         <input type="submit" value="Effectuer la requête Google analytics">
     </form>
     """
+
+    # Bouton pour récupérer le nombre de visiteurs
+    nombre_visiteurs_button = """
+    <form method="GET" action="/fetch-google-analytics-data">
+        <input type="submit" value="Récupérer le nombre de visiteurs">
+    </form>
+    """
     
-    return "logger page" + browser_log + textbox_form + google_button + google_analytics_button
+    
+    return "logger page" + browser_log + textbox_form + google_button + google_analytics_button  + nombre_visiteurs_button
 
 @app.route("/perform_google_request", methods=["GET", "POST"])
 def perform_google_request():
@@ -95,3 +108,37 @@ def perform_google_analytics_request():
         response_message = "Request to Google analytics failed."
 
     return str(response_message)
+
+@app.route('/fetch-google-analytics-data', methods=['GET'])
+def fetch_google_analytics_data():
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'form.json'
+    PROPERTY_GA4_ID = 'G-P8H5EB609C'
+    starting_date = "10daysAgo"
+    ending_date = "yesterday"
+
+    client = BetaAnalyticsDataClient()
+
+    # function qui retourne la requête de l'API Google Analytics
+    def get_visitor_count(client, property_id):
+        request = RunReportRequest(
+            property=f"properties/{property_id}",
+            date_ranges=[{"start_date": starting_date, "end_date": ending_date}],
+            metrics=[{"name": "activeUsers"}]
+        )
+        response = client.run_report(request)
+        return response
+
+    # récupère nombre visiteurs actifs
+    response = get_visitor_count(client, PROPERTY_GA4_ID)
+
+    # affiche nombre visiteurs actifs
+    if response and response.row_count > 0:
+        metric_value = response.rows[0].metric_values[0].value
+    else:
+        metric_value = "N/A"
+
+    return f'Number of active visitors : {metric_value}'
+
+if __name__ == '_main_':
+    app.run(debug=True)
